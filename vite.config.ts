76b8c601713583600b0e7ec656f599086bee9211
MsKing <2421152465@qq.com>
2026-01-19 17:54:45 +0800

    s

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
-   base: mode === 'prod' ? '/repo/' : '/repo/dev/',
+export default defineConfig(() => ({
+  base: process.env.VITE_BASE_PATH || '/',
   build: {
     outDir: 'dist',   // 👈 永远是 dist
     emptyOutDir: true
