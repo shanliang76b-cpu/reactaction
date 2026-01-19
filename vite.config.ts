44f1266b27e3b5e19f54a066bd50fa0fdd76be3b
MsKing <2421152465@qq.com>
2026-01-19 18:09:16 +0800

    main

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig(() => ({
-  // base: process.env.VITE_API_BASE || '/',
   base: '/repo/',
   plugins: [react()],
 }))
