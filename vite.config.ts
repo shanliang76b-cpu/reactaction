3ee6e1b6f359a2cc5eee6bde018f078e5a154cd3
MsKing <2421152465@qq.com>
2026-01-19 17:16:02 +0800

    x edit .env.production

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/repo/dev/', 
+export default defineConfig(({ mode }) => ({
+  base: mode === 'prod' ? '/repo/' : '/repo/dev/',
+  build: {
+    outDir: mode === 'prod' ? 'dist/prod' : 'dist/dev'
+  },
   plugins: [react()],
-})
+}))
