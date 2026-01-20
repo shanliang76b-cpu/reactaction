7d3aa6af5694a4b5afddaa781269fff9ee9db2de
MsKing <2421152465@qq.com>
2026-01-20 11:14:36 +0800

    5

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig  } from 'vite'
+import { defineConfig ,loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(() => {
+export default defineConfig(({mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
   return {
     plugins: [react()],
-    base: '/dev',
+    base: env.VITE_API_BASE || '/',
   }   
 })
